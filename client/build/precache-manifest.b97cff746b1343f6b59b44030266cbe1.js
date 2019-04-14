9ac54ad59404076194cd30257569298b4fd451fc
Suzanne Hunter <sue_huntr@yahoo.com>
2019-04-13 22:12:55 -0400

    npm run build

---
+++ client/build/precache-manifest.b97cff746b1343f6b59b44030266cbe1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c53b99236c169a8d303e",
+    "url": "/static/css/main.aa2455ad.chunk.css"
+  },
+  {
+    "revision": "c53b99236c169a8d303e",
+    "url": "/static/js/main.7232fa2d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "600417e774ac2c427bc3",
+    "url": "/static/js/2.ab1f5202.chunk.js"
+  },
+  {
+    "revision": "acc4495df1687d360429333373e6f4bc",
+    "url": "/index.html"
+  }
+];
